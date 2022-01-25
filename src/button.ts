efbd4df690e6b581dcc560c73880fa14e12848a5
fahrulseptiana <10472373+fahrulseptiana@users.noreply.github.com>
2022-01-25 08:50:34 +0700

    feat: connect var

--- src/button.ts
+++ src/button.ts
@@ -1,6 +1,12 @@
+import {buttonsBorderRadius, buttonsLgPaddingTop, buttonsLgPaddingBottom, buttonsLgPaddingLeft, buttonsLgPaddingRight} from './legion-tokens/output/js/logee-transportation'
+
 export const button = {
   alignItems: 'center',
-  borderRadius: '4px',
+  borderRadius: `${buttonsBorderRadius}px`,
+  paddingLeft: `${buttonsLgPaddingLeft}px`,
+  paddingRight: `${buttonsLgPaddingRight}px`,
+  paddingTop: `${buttonsLgPaddingTop}px`,
+  paddingBottom: `${buttonsLgPaddingBottom}px`,
   cursor: 'pointer',
   display: 'flex',
   lineHeight: '21px',
--- src/theme.ts
+++ src/theme.ts
@@ -4,14 +4,20 @@ import {makeTheme} from '@theme-ui/css/utils'
 import { merge, button } from '.'
 import { Theme } from '@theme-ui/css/dist/declarations/src/types'
 
-import {buttonsSolidBackgroundColor} from './legion-tokens/output/js/logee-transportation'
+import {buttonsSolidBackgroundColor, buttonsSoftBackgroundColor, buttonsSolidFontFamily, buttonsSoftFontFamily} from './legion-tokens/output/js/logee-transportation'
 
 const obj: Theme = {...merge(theme, {
   buttons: {
     solid: {
       ...button,
       backgroundColor: buttonsSolidBackgroundColor,
-    }
+      fontFamily: buttonsSolidFontFamily
+    },
+    soft: {
+      ...button,
+      backgroundColor: buttonsSoftBackgroundColor,
+      fontFamily: buttonsSoftFontFamily
+    },
   }
 })}
 
