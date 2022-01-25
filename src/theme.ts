297a6363a06fd16572dff03701a4c468b5c38150
fahrulseptiana <10472373+fahrulseptiana@users.noreply.github.com>
2022-01-25 09:06:06 +0700

    update: state

--- src/theme.ts
+++ src/theme.ts
@@ -4,19 +4,43 @@ import {makeTheme} from '@theme-ui/css/utils'
 import { merge, button } from '.'
 import { Theme } from '@theme-ui/css/dist/declarations/src/types'
 
-import {buttonsSolidBackgroundColor, buttonsSoftBackgroundColor, buttonsSolidFontFamily, buttonsSoftFontFamily} from './legion-tokens/output/js/logee-transportation'
+import {buttonsSolidBackgroundColor, buttonsSoftBackgroundColor, buttonsSolidFontFamily, buttonsSoftFontFamily, buttonsSolidHoverBackgroundColor, buttonsSolidActiveBackgroundColor, buttonsSolidFocusBackgroundColor, buttonsSolidDisabledBackgroundColor, buttonsSoftHoverBackgroundColor, buttonsSoftActiveBackgroundColor, buttonsSoftFocusBackgroundColor, buttonsSoftDisabledBackgroundColor} from './legion-tokens/output/js/logee-transportation'
 
 const obj: Theme = {...merge(theme, {
   buttons: {
     solid: {
       ...button,
       backgroundColor: buttonsSolidBackgroundColor,
-      fontFamily: buttonsSolidFontFamily
+      fontFamily: buttonsSolidFontFamily,
+      '&:hover': {
+        backroundColor: buttonsSolidHoverBackgroundColor
+      },
+      '&:active': {
+        backgroundColor: `${buttonsSolidActiveBackgroundColor} !important`
+      },
+      '&:focus': {
+        backgroundColor: `${buttonsSolidFocusBackgroundColor} !important`
+      },
+      '&:disabled': {
+        backgroundColor: `${buttonsSolidDisabledBackgroundColor} !important`
+      }
     },
     soft: {
       ...button,
       backgroundColor: buttonsSoftBackgroundColor,
-      fontFamily: buttonsSoftFontFamily
+      fontFamily: buttonsSoftFontFamily,
+      '&:hover': {
+        backroundColor: buttonsSoftHoverBackgroundColor
+      },
+      '&:active': {
+        backgroundColor: `${buttonsSoftActiveBackgroundColor} !important`
+      },
+      '&:focus': {
+        backgroundColor: `${buttonsSoftFocusBackgroundColor} !important`
+      },
+      '&:disabled': {
+        backgroundColor: `${buttonsSoftDisabledBackgroundColor} !important`
+      }
     },
   }
 })}
